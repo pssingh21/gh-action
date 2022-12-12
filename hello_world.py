dd9993c5a7450e9e3e7bcef14993b6eb6dcca990
pssingh21 <peeyushmansingh@gmail.com>
2022-12-12 13:19:14 +0100

    Env variable changed

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,4 @@ import os
 
 print('Hello ' + sys.argv[1])
 time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"::set-output name=time::{'time='+time}")
+print(f"::set-output name=$GITHUB_OUTPUT::{'time='+time}")
