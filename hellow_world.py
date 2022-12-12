daae611315444a21fe4e63f400f1969035134cec
pssingh21 <peeyushmansingh@gmail.com>
2022-12-12 12:33:18 +0100

    My first action is ready

---
+++ hellow_world.py
@@ -0,0 +1,7 @@
+import sys
+import datetime
+import os
+
+print('Hello ' + sys.argv[1])
+time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"::set-output name=GITHUB_OUTPUT::{'time='+time}")
