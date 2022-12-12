201776a9287e22922cc1289b83746a7a73e35879
pssingh21 <peeyushmansingh@gmail.com>
2022-12-12 15:39:24 +0100

    Code cleanup

--- src/gh_action_poc_pssingh21/hello_world.py
+++ src/gh_action_poc_pssingh21/hello_world.py
@@ -4,7 +4,6 @@ import os
 
 print('Hello ' + sys.argv[1])
 time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-# print(f"::set-output name=$GITHUB_OUTPUT::{'time='+time}")
-name = 'time'
+
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={time}', file=fh)
+    print(f'time={time}', file=fh)
