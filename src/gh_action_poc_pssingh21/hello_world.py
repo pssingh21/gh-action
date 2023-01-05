7ddf3065bbc1b24aff498a1feb2c96f30114ff26
pssingh21 <peeyushmansingh@gmail.com>
2023-01-05 14:05:27 +0100

    Package install

--- src/gh_action_poc_pssingh21/hello_world.py
+++ src/gh_action_poc_pssingh21/hello_world.py
@@ -5,8 +5,8 @@ import os
 def sayHello():
     print('Hello World')
 
-print('Hello ' + sys.argv[1])
-time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# print('Hello ' + sys.argv[1])
+# time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'time={time}', file=fh)
+# with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+#     print(f'time={time}', file=fh)
